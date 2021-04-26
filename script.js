4ff2d151390e0572042b8015f5a8d6f72a5ac4fd
Brian Palay <bpplaysguitar@hotmail.com>
2021-04-26 09:24:37 -0500

    started making separate functions for different arrays

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");  
+let generateBtn2 = document.querySelector("#generate2");  
 
 // Write password to the #password input
 function writePassword() {
@@ -21,58 +22,107 @@ function writePassword() {
 
   useSpecial = confirm("Use special characters?");
 
-console.log(passwordLength);
-console.log(useUppercase);
-console.log(useLowercase);
-console.log(useSpecial);
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+generateBtn2.addEventListener("click", randomLuns);
 
-function getRandomLowercase() {
-  let lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  let randomLowercaseLetter = lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)];
-  console.log(randomLowercaseLetter);
+// function getRandomLowercase() {
+//   let lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//   let randomLowercaseLetter = lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)];
+//   console.log(randomLowercaseLetter);
+//   }
+
+//   function getRandomUppercase() {
+//     let uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//     let randomUppercaseLetter = uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)];
+//     console.log(randomUppercaseLetter);
+//     }
+
+//   function getRandomSpecial() {
+//     let specialCharacters = ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+//     let randomSpecialCharacter = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+//     console.log(randomSpecialCharacter);
+//     }
+
+// generateBtn.addEventListener("click", getRandomLowercase);
+
+// generateBtn.addEventListener("click", getRandomUppercase);
+
+// generateBtn.addEventListener("click", getRandomSpecial);
+
+
+//___________________________________________
+// Create arrays of all possible non-order-specific combinations of lower, upper, number, and special so in the end random characters will be selected from the appropriate array.
+// combinations of luns
+// luns
+// lun
+// lus
+// lns
+// uns
+// lu
+// ln
+// ls
+// un
+// us
+// ns
+// l
+// u
+// n 
+// s
+
+// lower upper number special 
+let luns = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+
+// lower upper number
+let lun = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+// lower upper 
+let lu = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+// lower 
+let l = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+// lower upper special 
+let lus = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+
+// lower number special 
+let lns = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+
+// upper number special 
+let uns = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+
+// lower number
+let ln = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+// lower special 
+let ls = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+
+// upper number 
+let un = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+// upper special 
+let us = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+
+// number special 
+let ns = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+
+// upper
+let u = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+// number 
+let n = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+// special 
+let s = ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+//___________________________________________
+
+
+function randomLuns() {
+
+  for (let index = 0; index < 2; index++) {
+    lunsCharacter = luns[Math.floor(Math.random() * luns.length)];
+    console.log(lunsCharacter);   
   }
-
-  function getRandomUppercase() {
-    let uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    let randomUppercaseLetter = uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)];
-    console.log(randomUppercaseLetter);
-    }
-
-  function getRandomSpecial() {
-    let specialCharacters = ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
-    let randomSpecialCharacter = specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
-    console.log(randomSpecialCharacter);
-    }
-
-generateBtn.addEventListener("click", getRandomLowercase);
-
-generateBtn.addEventListener("click", getRandomUppercase);
-
-generateBtn.addEventListener("click", getRandomSpecial);
-
-
-
-// arraycombinations of a b c d
-// abcd
-// abc
-// abd
-// acd
-// bcd
-// ab
-// ac
-// ad
-// bc
-// bd
-// cd 
-// a 
-// b
-// c
-// d
-
-
-
+}
